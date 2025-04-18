6d86df8d990b2f025ef3df8a74082bc35555ef8e
Bozeyy <spereaux2@gmail.com>
2025-04-18 13:20:52 +0300

    chande title of app

--- csflash/src/components/Header.tsx
+++ csflash/src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <h1>CSFlash</h1>
+        <h1>CSFLASH</h1>
         <button className="login-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
